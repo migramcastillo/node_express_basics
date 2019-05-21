2ff51994c545e7c84ff182586754fcf0c7d379bd
Miguel Castillo <migramcastillo@gmail.com>
2019-05-21 10:32:47 -0500

    Ejemplo asyncrono

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 const routes = require("./routes");
 const path = require("path");
 
+const crypto = require("crypto");
+const fs = require("fs");
+const https = require("https");
+
 //Body from post request
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -13,12 +17,63 @@ app.set('view engine', 'hbs');
 
 app.use(express.static('./public'));
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/async", (req, res) => {
+
+    let operations = 8;
+    const start = Date.now();
+
+    const asyncHandler = () => {
+        operations--;
+
+        if(operations === 0){
+            return res.send(`Hello World! on ${Date.now() - start}`);
+        }
+    };
+
+    //I don't care, I'm a OS operation
+    https.get("https://www.google.com", (res) => {
+        console.log("Reading google on: ", Date.now() - start);
+        asyncHandler();
+    });
+
+    // Asynchronous request, what will show first?
+    fs.readFile("./home.html", (err, data) => {
+        if(err) throw new Error(err);
+
+        console.log("FS:", Date.now() - start);
+        asyncHandler();
+    });
+
+    for (let index = 1; index <= 6; index++) {
+        crypto.pbkdf2('mypassword', 'mysalt', 100000, 512, 'sha512', () => {
+            console.log(`Crypto ${index}: `, Date.now() - start);
+            asyncHandler();
+        });
+    }
+});
+
+app.get("/sync", (req, res) => {
+    (async (callback) => {
+        const start = Date.now();
+
+        const result = await https.get("https://www.google.com");
+        console.log("Http async ", Date.now() - start);
+    
+        const file = await fs.readFileSync("./home.html");
+        console.log("File async ", Date.now() - start);
+    
+        for (let index = 1; index <= 4; index++) {
+            const cryptoResult = await crypto.pbkdf2Sync('mypassword', 'mysalt', 100000, 512, 'sha512');
+            console.log(`Crypto ${index}: `, Date.now() - start);
+        }
+        callback(start);
+    })((initTime) => {
+        return res.send(`Hello World! on ${Date.now() - initTime}`);
+    });
 });
 
 app.get("/home", (req, res) => {
-    res.sendFile(path.resolve("./public/index.html"));
+    res.sendFile(path.resolve("./public/home.html"));
 });
 
 app.use(routes);
